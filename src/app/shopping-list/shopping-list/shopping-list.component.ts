0119e9012258dd5db303749cb6c7bd0ebdf0a70e
Adam Cross <adamfgcross@gmail.com>
2021-10-05 14:42:50 -0400

    add some shopping list elements

--- src/app/shopping-list/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list/shopping-list.component.ts
@@ -8,7 +8,10 @@ import { Ingredient } from 'src/app/model/ingredient.model';
 })
 export class ShoppingListComponent implements OnInit {
 
-  ingredients : Ingredient[] = [];
+  ingredients : Ingredient[] = [
+    new Ingredient("Apples", 5),
+    new Ingredient("Tomatos", 10)
+  ];
   constructor() { }
 
   ngOnInit(): void {
